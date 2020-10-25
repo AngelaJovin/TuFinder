fb03d516c2f1b74281f3ff215f15064a8c7e1f4b
mushi175 <jmushi175@gmail.com>
2020-10-25 23:54:13 +0300

    changes

--- TuFinder/TuFinder/includes/loginSignUP.php
+++ TuFinder/TuFinder/includes/loginSignUP.php
@@ -75,7 +75,7 @@
                             <input type="email" class="form-control mb-3" id="loginEmail" name="loginEmail" placeholder="Email">
                         </div>
                     <div class="col-12">
-                        <input type="password" class="form-control mb-3" id="loginPassword" name="loginPassword" placeholder="Password" value="323">
+                        <input type="password" class="form-control mb-3" id="loginPassword" name="loginPassword" placeholder="Password" value="">
                     </div>
                     <div class="col-12">
                         <input type="submit" class="btn btn-primary" name="login" value="SIGN IN" >
