7e8a87321fa6c5944a39b153b6c639372e0a37ab
mushi175 <jmushi175@gmail.com>
2020-10-25 23:45:30 +0300

    for db

--- TuFinder/TuFinder/includes/db.php
+++ TuFinder/TuFinder/includes/db.php
@@ -18,7 +18,25 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 else{
-  echo "connected";
+  // echo "connected";
+  // $sql="CREATE TABLE `registration` (
+  //   `id` int(11) NOT NULL,
+  //   `email` varchar(100) DEFAULT NULL,
+  //   `name` varchar(200) DEFAULT NULL,
+  //   `password` varchar(200) DEFAULT NULL,
+  //   `status` varchar(50) DEFAULT NULL,
+  //   `subject1` varchar(100) DEFAULT NULL,
+  //   `subject2` varchar(100) DEFAULT NULL,
+  //   `subject3` varchar(100) DEFAULT NULL,
+  //   `bio` text DEFAULT NULL,
+  //   `contact` varchar(255) DEFAULT NULL,
+  //   `profilePicture` varchar(255) DEFAULT NULL
+  // ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  // ";
+//$result_set=mysqli_query($conn,$sql);
+// if(!$result_set){
+//   die("Database Query failed");
+//  }
 }
 
 ?>
