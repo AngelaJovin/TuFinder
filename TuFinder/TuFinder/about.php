c0fd6004742ac0b9e462f73722df74f24d2b2436
AngelaJovin <67833415+AngelaJovin@users.noreply.github.com>
2020-11-09 00:28:57 +0300

    Update about.php

--- TuFinder/TuFinder/about.php
+++ TuFinder/TuFinder/about.php
@@ -3,12 +3,9 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
   <meta charset="utf-8">
-  <title>TuFinder</title>
+  <title>ScuHub</title>
 
   <!-- mobile responsive meta -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -39,78 +36,111 @@
 
 <body>
   
-
-<!-- header -->
+!-- header -->
 <header class="fixed-top header">
-  <!-- top header -->
-  <?php
-
-require_once("includes/topHeader.php");
-?>
-
-
-  <!-- navbar -->
-  <div class="navigation w-100">
-  <?php
-    require_once("includes/navigation.php");
-
-    ?>
-  </div>
-
   
+  <!-- navbar -->
+  <div class="navigation w-10">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light p-0">
+        <a class="navbar-brand" href="index.html"> <h2 class="section-title text-white">ScuHub</h2></a>
+        <button class="navbar-toggler rounded-1" type="button" data-toggle="collapse" data-target="#navigation"
+          aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navigation">
+          <ul class="navbar-nav ml-auto text-center">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+
+            <li class="nav-item active">
+              <a class="nav-link" href="about.html">About</a>
+            </li>
+        
+            <li class="nav-item">
+              <a class="nav-link" href="teacher.html">Tutors</a>
+            </li>
+              
+             <li class="nav-item dropdown view">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                Others
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="forms.html">Registration Forms</a>
+                <a class="dropdown-item" href="studymaterials.html">Study Materials</a>
+               
+                
+              </div>
+            </li>
+            
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
 </header>
 <!-- /header -->
 
 
-
-<!-- Modal for login and signup is here -->
-<?php
-
-require_once("includes/loginSignUP.php");
-?>
-<!--page title-->
-<section class="page-title-section overlay" data-background="images/backgrounds/page-title.jpg">
+<!-- page title -->
+<section class="page-title-section overlay bg-footer">
   <div class="container">
     <div class="row">
       <div class="col-md-8">
         <ul class="list-inline custom-breadcrumb">
-          <li class="list-inline-item"><a class="h2 text-primary font-secondary">About Us</a></li>
-          <li class="list-inline-item text-white h3 font-secondary @@nasted"></li>
-        </ul>
-         <p class="text-lighten">TuFinder helps you connect with the best private tutors  </br>near you in any subject.Contact a professional/Peer tutor to </br>schedule your lessons start in person or online tutoring.</p>
-         <div class="row">
-          <div class="col-lg-6">
-          
- <ul class="list-inline custom-breadcrumb">
-          <li class="h2 text-primary font-secondary" >Our Services</li>
-          <li class="list-inline-item text-white h3 font-secondary @@nasted"></li>
-        </ul>
-           
-            <div >
-              <h4 class="text-lighten">PARENTS</h4>
-            <p class="text-white"><em>Sign up and create account where you will be able to edit your profile,provide much information as possible to help you access the right tutors for your child </em></p>
-              
-            </div>
-             <div >
-              <h4  class="text-lighten">STUDENTS</h4>
-              <p class="text-white"><em>Sign up and create account where you will be able to edit your profile,provide much information as possible to help you access the right tutors for yourself</em></p>
-              
-            </div>
-             <div >
-              <h4  class="text-lighten">TUTORS</h4>
-              <p class="text-white"><em>Sign up and create account where you will be able to edit your profile,provide much information as possible to help parents and students access you easily</em></p>
-             
-            </div>
-          </div>
+          <li class="list-inline-item "><a class="h1 text-primary font-secondary" >About Us </a></li>
          
-            </div>
-            <h5>NOTE</h5>
-             <p class="text-lighten">Parents and Students will be able to contact the </br>Tutors only after they sign up and Create accounts</br> and Only Tutors with Accounts will be able to see the </br>students and parents that need the Tutoring services.</p>
+        </ul>
+          <p class="text-lighten">ScuHub is a platform for everybody;Students,Parents and Teachers too.It makes learning easy and anywhere.Enjoy our Services.</p>
+       
       </div>
     </div>
   </div>
 </section>
 <!-- /page title -->
+
+
+
+
+<!-- extra -->
+<section class="section ">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h6 class="text-gray font-secondary mb-0">Access Study Materials</h6>
+        <h2 class="section-title text-gray">Get reliable study materials like pamphlets,books and the like from our teachers. </h2>
+        <a href="studymaterials.html" class="btn btn-primary-outline">Explore more</a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- /extra -->
+
+<section class="section overlay bg-footer">
+  <div class="container">
+    <div class="row">
+     <div class="col-12 text-center">
+        <h6 class="text-gray font-secondary mb-0">Get enrolled in schools easily</h6>
+        <h2 class="section-title text-gray">Wherever you are you can access school registration forms,fill and submit them.</h2>
+        <a href="books.html" class="btn btn-primary-outline">Explore more</a>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section " id="tutoring">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h6 class="text-white font-secondary mb-0">Join Our tutoring community</h6>
+        <h2 class="section-title text-white">Whatever the subject we can help,wherever you are our services are available.</h2>
+        <a href="teacher.html" class="btn btn-primary-outline">Join now</a>
+      </div>
+    </div>
+  </div>
+</section>
 
 <!-- about -->
 <section class="section">
@@ -124,27 +154,66 @@ require_once("includes/loginSignUP.php");
 </section>
 <!-- /about -->
 
+<!-- banner-feature -->
+<section class="section">
+  
+  <div class="container">
+            <div class="col-12">
+         <center><h2 class="section-title">Our Services</h2></center>
+        </div>
+        <div class="row  feature-blocks bg-white justify-content-between">
+          
+          <div class="col-sm-6 col-xl-5 mb-xl-5 mb-lg-3 mb-4 text-center ">
+            <i class="ti-book mb-xl-4 mb-lg-3 mb-4 feature-icon"></i>
+            <h3 class="mb-xl-4 mb-lg-3 mb-4">Study Materials</h3>
+            <p>Students and Teachers can share and buy study materials like pamphlets,books,projects and reports from any subject</p>
+          </div>
+          <div class="col-sm-6 col-xl-5 mb-xl-5 mb-lg-3 mb-4 text-center ">
+            <i class="ti-blackboard mb-xl-4 mb-lg-3 mb-4 feature-icon"></i>
+            <h3 class="mb-xl-4 mb-lg-3 mb-4">Tutoring</h3>
+            <p>You can connect with Tutors near you or from other areas and contact them for help in any subject.Sign up as a parent or student to begin scheduling your lessons.
+            </p>
+          </div>
+           <div class="col-sm-6 col-xl-5 mb-xl-5 mb-lg-3 mb-4 text-center ">
+            <i class="ti-write mb-xl-4 mb-lg-3 mb-4 feature-icon"></i>
+            <h3 class="mb-xl-4 mb-lg-3 mb-4">Registration forms</h3>
+            <p>Parents and students can get registration forms of primary,secondary and high schools from our website and easily fill them and submit them.
+            </p>
+          </div>
+           <div class="col-sm-6 col-xl-5 mb-xl-5 mb-lg-3 mb-4 text-center ">
+            <i class="ti-agenda mb-xl-4 mb-lg-3 mb-4 feature-icon"></i>
+            <h3 class="mb-xl-4 mb-lg-3 mb-4">Others</h3>
+            <p>Parents and Students will be able to contact the Tutors only after they sign up and Create accounts and Only Tutors with Accounts will be able to see the students and parents that need the Tutoring services.
+            </p>
+          </div>
+       
+        </div>
+      </div>
+</section>
+<!-- /banner-feature -->
 
-<!-- why Us -->
+<!--why us-->
 <section class="section bg-cover" data-background="images/backgrounds/success-story.jpg">
   <div class="container">
     <div class="row">
-      
+     
       <div class="col-lg-6 col-sm-8">
         <div class="bg-white p-5">
           <h2 class="section-title">Why Us</h2>
-          <p>With Tufinder there is a wide choice of Tutors whether for online or in person tutoring and also the whole process of connecting with a Tutor is simple,quick and straight forward.You can contact as many tutors as you like and select the perfect Tutor for yourself or for your group.</p>
-          <p>Whatever the subject we can help.Wherever you are,our services are accessible.Our services are for everyone.Parents,students and Tutors.</p>
-           <a href="teacher.html" class="btn btn-primary-outline">Find a Tutor</a>
+          <p>With ScuHub there is a wide choice of Tutors whether for online or in person tutoring and also the whole process of connecting with a Tutor is simple,quick and straight forward.
+          You can contact as many tutors as you like and select the perfect Tutor for yourself or for your group.
+      Also registration forms for different schools can be accessed easily,fill and submit in a simpler way. </p>
+        
         </div>
       </div>
     </div>
-  </div>.
+  </div>
 </section>
-<!-- /why Us -->
+<!-- /why us-->
+
 
 <!-- team -->
-<section class="section">
+<section class="section" id="team">
     <div class="container">
       <div class="row">
         <div class="col-12">
@@ -161,7 +230,7 @@ require_once("includes/loginSignUP.php");
               <div class="d-flex justify-content-between">
                 <span>System Designer</span>
                 <ul class="list-inline">
-                 
+                
                   <li class="list-inline-item"><a class="text-color" href="https://github.com/AngelaJovin"><i class="ti-github"></i></a></li>
                   <li class="list-inline-item"><a class="text-color" href="https://www.instagram.com/_being_angie_/"><i class="ti-instagram"></i></a></li>
                 </ul>
@@ -183,7 +252,7 @@ require_once("includes/loginSignUP.php");
                 <span>Finance Manager</span>
                 <ul class="list-inline">
                   <li class="list-inline-item"><a class="text-color" href="https://web.facebook.com/dorice.kakwaya"><i class="ti-facebook"></i></a></li>
-              
+               
                   <li class="list-inline-item"><a class="text-color" href="https://www.instagram.com/n.i.v.e.r.a/"><i class="ti-instagram"></i></a></li>
                 </ul>
               </div>
@@ -202,7 +271,7 @@ require_once("includes/loginSignUP.php");
               <div class="d-flex justify-content-between">
                 <span>System Developer</span>
                 <ul class="list-inline">
-                 
+                
                   <li class="list-inline-item"><a class="text-color" href="https://github.com/itsmushi"><i class="ti-github"></i></a></li>
                  <li class="list-inline-item"><a class="text-color" href="https://www.instagram.com/itsmushijames/"><i class="ti-instagram"></i></a></li>
                 </ul>
@@ -223,7 +292,7 @@ require_once("includes/loginSignUP.php");
               <div class="d-flex justify-content-between">
                 <span>System Developer</span>
                 <ul class="list-inline">
-                  
+                 
                   <li class="list-inline-item"><a class="text-color" href="https://github.com/domel06"><i class="ti-github"></i></a></li>
                  <li class="list-inline-item"><a class="text-color" href="https://www.instagram.com/domel_bura/"><i class="ti-instagram"></i></a></li>
                 </ul>
@@ -238,10 +307,77 @@ require_once("includes/loginSignUP.php");
   <!-- /team -->
 
 
+<!-- contact -->
+<section class="section bg-white" id="contact">
+  <div class="container">
+   
+     <div class="row feature-blocks bg-gray justify-content-between">
+       <div class="col-12">
+         <center><h2 class="section-title">Contact Us</h2>
+          <p>Feel free to contact us if you have any concern,just fill the form below.</p></center>
+        </div>
+
+      <div class="col-12">
+        <form action="#">
+          <input type="text" class="form-control mb-3" id="name" name="name" placeholder="Your Name">
+          <input type="email" class="form-control mb-3" id="mail" name="mail" placeholder="Your Email">
+          <input type="text" class="form-control mb-3" id="subject" name="subject" placeholder="Subject">
+          <textarea name="message" id="message" class="form-control mb-3" placeholder="Your Message"></textarea>
+          <center><button type="submit" value="send" class="btn btn-sm btn-primary">SEND MESSAGE</button></center>
+          <p></p>
+        </form>
+      </div>
+     </div>
+    </div>
+ 
+</section>
+<!-- /contact -->
+
+
+
+
 <!-- footer -->
-<footer>
-  
+ <!-- footer content -->
+ <footer>
+  <div class="footer bg-footer section border-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-sm-8 mb-5 mb-lg-0">
+          <!-- logo -->
+          <a class="logo-footer" href="index.html"> <h2 class="section-title text-white">ScuHub</h2></a>
+          <ul class="list-unstyled">
+            <li class="mb-2">Sayansi,Kijitonyama Dar es Salaam Tanzania</li>
+            <li class="mb-2">+255 657 308 690</li>
+            <li class="mb-2">scuhub.co.tz</li>
+          </ul>
+        </div>
+        <!-- company -->
+        <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
+         
+          <ul class="list-unstyled">
+             <li class="mb-3"><a class="text-color" href="index.html">Home</a></li>
+            <li class="mb-3"><a class="text-color" href="about.html">About Us</a></li>
+            <li class="mb-3"><a class="text-color" href="about.html #team">Our Team</a></li>
+            <li class="mb-3"><a class="text-color" href="index.html #contact">Contact</a></li>
+          
+          </ul>
+        </div>
+
+
+        <!-- links -->
+        <div class="col-lg-2 col-md-3 col-sm-4 col-6 mb-5 mb-md-0">
+         
+          <ul class="list-unstyled">
+            <li class="mb-3"><a class="text-color" href="advertisewithus.html">Sell with us</a></li>
+            <li class="mb-3"><a class="text-color" href="privacypolicy.html">Privacy Policy</a></li>
+            <li class="mb-3"><a class="text-color" href="termsofuse.html">Terms of Use</a></li>
+            <li class="mb-3"><a class="text-color" href="faqs.html">FAQs</a></li>
+          </ul>
+        </div>
        
+      </div>
+    </div>
+  </div>
   <!-- copyright -->
   <div class="copyright py-4 bg-footer">
     <div class="container">
@@ -252,9 +388,9 @@ require_once("includes/loginSignUP.php");
               var CurrentYear = new Date().getFullYear()
               document.write(CurrentYear)
             </script> 
-             <a href="https://Tu-finder.com">TuFinder.com</a>.All Rights Reserved.</p>
+            ©  <a href="https://ScuHub.co.tz">ScuHub.co.tz</a> . All Rights Reserved.</p>
         </div>
-        
+       
       </div>
     </div>
   </div>
